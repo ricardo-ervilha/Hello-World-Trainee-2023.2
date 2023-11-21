7c57c89d70e431504c451074526b97c2b86ddf8e
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2023-11-21 10:11:12 -0300

    Passa id nos ícones dos modais

--- app/views/admin/icons.php
+++ app/views/admin/icons.php
@@ -1,14 +1,13 @@
-
 <td>
-								<button id="openUserModalBtn" class="icon-visualizar"><img src="../../../public/assets/icon-ver.png"></button>
+								<button id="openUserModalBtn<?=$row['id']?>" class="icon-visualizar"><img src="../../../public/assets/icon-ver.png"></button>
 
-								<?php require('./app/views/admin/modalViewUser.html') ?>
+								<?php require('./app/views/admin/modalViewUser.php') ?>
 
-								<button id="open-modal" class="icon-editar"><img src="../../../public/assets/icon-editar.png"></button>
+								<button id="open-modal<?=$row['id']?>" class="icon-editar"><img src="../../../public/assets/icon-editar.png"></button>
 
-								<?php require('./app/views/admin/modal_edit_user.html') ?>
+								<?php require('./app/views/admin/modal_edit_user.php') ?>
 
-								<button onclick="document.getElementById('id01').style.display='block'" class="icon-excluir"><img src="../../../public/assets/icon-delete.png"></button>
+								<button id="delModal<?=$row['id']?>" class="icon-excluir"><img src="../../../public/assets/icon-delete.png"></button>
 
-								<?php require('./app/views/admin/modal_delete_user.html') ?>
+								<?php require('./app/views/admin/modal_delete_user.php') ?>
 </td>
