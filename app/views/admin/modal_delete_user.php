628433029856bb109731a7f9e460b5b0cc3396a8
Paulo Aquino <paulo.aquino@estudante.ufjf.br>
2023-11-29 01:43:32 -0300

    Crud Delete Users + PostIn

--- app/views/admin/modal_delete_user.php
+++ app/views/admin/modal_delete_user.php
@@ -5,8 +5,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Kelly+Slab&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 </head>
 <body>
-<div id="id01<?=$function->id?>" class="modal-d" style="display: none;">
-    <form class="modal-d-content" action="/action_page.php">
+<div id="id01<?=$function->id?>" class="modal" style="display: none;">
+    <form class="modal-d-content" action="delete" method="POST">
+    <input type="hidden" value="<?= $function->id ?>" name="id">
         <div class="container-d">
             <span class="close-delete"
                 title="Close Modal">&times;</span>
