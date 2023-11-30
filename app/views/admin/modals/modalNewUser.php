e3cc362251a2e00a01dc4fdd51188d1d5e36766d
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2023-11-30 16:04:19 -0300

    modalnewuser

--- app/views/admin/modals/modalNewUser.php
+++ app/views/admin/modals/modalNewUser.php
@@ -1,41 +1,36 @@
 <div class="modal" id="modal" style="display:none;">
-    <div class="modal-newuser-content">
-        <form action="users/create" method="POST">
+    <div class="modal-content">
         <span class="close">&times</span>
-        <div class="modal-newuser-head" id="cabecalho">
+        <div class="modal-head-new-user" id="cabecalho">
             <h1> Novo Usuário</h1>
         </div>
 
-        <div class="modal-newuser-content-body">
+        <div class="content-body">
             <div class="name" id="name">
                 <h2>Nome: </h2>
-                <input class="campo-first-name" type="text" placeholder="Primeiro nome" name="name">
-                <input class="campo-last-name" type="text" placeholder="Sobrenome" name="sobrenome">
+                <input class="campo-first-name" type="text" placeholder="Primeiro nome">
+                <input class="campo-last-name" type="text" placeholder="Sobrenome">
             </div>
 
             <div class="campo-phone" id="phone">
                 <h2>Telefone:</h2>
-                <input type="tel" name="phone">
+                <input type="tel">
             </div>
 
             <div class="campo-email" id="email">
                 <h2>E-mail:</h2>
-                <input type="email" name="email">
+                <input type="email">
             </div>
 
             <div class="campo-senha" id="senha">
                 <h2>Senha:</h2>
-                <input type="password" name="password">
+                <input type="password">
             </div>
 
-            <div class="div-cadastrar" id="cadastrar">
+            <div class="cadastrar" id="cadastrar">
                 <button>Cadastrar</button>
             </div>
         </div>
-        </form>
 
     </div>
 </div>
-
-<link rel="stylesheet" href="../../../public/css/modalNewUser.css">
-<script src="../../../public/js/modalNewUser.js"></script>
