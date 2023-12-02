7404214ae0b266ad0207da84419eb7ed38d9c392
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2023-12-01 23:16:59 -0300

    ajeita img post view

--- app/views/admin/listaPosts.view.php
+++ app/views/admin/listaPosts.view.php
@@ -1,31 +1,42 @@
 <!DOCTYPE html>
+
 <html>
+
 	<head>
+
 		<meta charset="utf-8">
 		<title>Posts - Hello World</title>
+
 		<link rel="stylesheet" href="../../../public/css/listaUsers.css">
+        
 		<link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
 	</head>
+
+
 	<body>
 		<div class="container">
+
 			<h1 class="h1-list-users">Lista de Posts</h1>
+
 			<div class="new-user">
 					<button id="add-user">
 						<div class="button-new user"><img src="../../../public/assets/novoPost.png"></div>
 						<div class="text-new-user"><h2>Novo Post</h2></div>
 					</button>
 
-					<?php require ('./app/views/admin/modals/modalNewPost.php'); ?>
-
+					<?php require('./app/views/admin/modals/modalNewPost.php') ?>
 			</div>
+
 			<div class="div-tabela">
+
 				<table class="tabela">
 					<thead>
-						<tr class="cabeçalho">	
+						<tr class="cabeçalho">
                         <th>Título</th>
-                        <th>Autor</th>
+                        <th>Usuário</th>
                         <th>Data de Modificação</th>
                         <th>Ações</th>
 						</tr>
@@ -41,10 +52,13 @@
 			
 			<?php endforeach; ?>
 
-			</tbody>
-			</table>
+		</tbody>
+		</table>
+
+			</div>
+		
 		</div>
-		</div>
+
 		<script src="../../../public/js/allModals.js"></script>
 	</body>
 </html>
