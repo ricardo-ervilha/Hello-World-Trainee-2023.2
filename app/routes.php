9869cf6543229adf6ddf751a15c303b8b963e94a
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2023-12-01 23:01:08 -0300

    routes

--- app/routes.php
+++ app/routes.php
@@ -4,19 +4,15 @@ use App\Core\Router;
 
 //Rotas da aplicação
 
-$router->get('listaDeUsuarios', 'UserController@usersView');
-$router->get('listaDePosts', 'PostController@postsView');
-
-//Login
-
 $router->get('login', 'LoginController@login');
 $router->get('dashboard', 'LoginController@inicio');
-
-//Create
 
 $router->post('users/create', 'UserController@usersCreate');
 $router->post('posts/create', 'PostController@postsCreate');
 
-//Delete
+$router->get('listaDeUsuarios', 'UserController@usersView');
+$router->get('listaDePosts', 'PostController@postsView');
+
+//deleteUsers
 $router->post('deleteUser', 'UserController@delete');
-$router->post('deletePost', 'PostController@postsDelete');
+$router->post('deletePost', 'PostController@delete');
