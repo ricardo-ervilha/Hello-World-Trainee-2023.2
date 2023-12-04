c64a90c6b7c3fae8c35b0b450942830de7315a2c
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2023-12-03 21:58:38 -0300

    atualiza login page

--- app/views/site/login.view.php
+++ app/views/site/login.view.php
@@ -1,42 +1,7 @@
 <?php
-include('connection.php');
 
-if(isset($_POST['email']) || isset($_POST['senha'])) {
+include('logout.php');
 
-    if(strlen($_POST['email']) == 0) {
-        echo "Preencha seu e-mail";
-    } else if(strlen($_POST['senha']) == 0) {
-        echo "Preencha sua senha";
-    } else {
-
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
-
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
-
-        $quantidade = $sql_query->num_rows;
-
-        if($quantidade == 1) {
-            
-            $usuario = $sql_query->fetch_assoc();
-
-            if(!isset($_SESSION)) {
-                session_start();
-            }
-
-            $_SESSION['id'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];
-
-            header("Location: painel.php");
-
-        } else {
-            echo "Falha ao logar! E-mail ou senha incorretos";
-        }
-
-    }
-
-}
 ?>
 
 <!DOCTYPE html>
@@ -59,7 +24,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
     
 
     <div class="tela-login" >
-        <form action="inicio" method="POST">
+        <form action="/login" method="POST">
             <div class=logo>
                 <img src="../../../public/assets/logo_hello_world.png" alt="Logo Hello World">
             </div>
@@ -80,7 +45,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
             
             <div class="button-entrar">
                 <button type="submit" >Entrar</a></button>
-        </div>
+            </div>
         </form>
   
     </div>
