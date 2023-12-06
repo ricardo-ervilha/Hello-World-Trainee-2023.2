3480894dc73a3fe033dead23883d0a3796c2e1bc
João Pedro Daniel <joaopedro.daniel@estudante.ufjf.br>
2023-12-06 11:50:38 -0300

    ajustes sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -5,11 +5,11 @@ const list = document.querySelectorAll(".list");
 let podeFechar = true;
 
 toggle.addEventListener('click', () => {
-    sidebar.classList.toggle("close");
+    sidebar.classList.toggle("close-sidebar");
 
     list.forEach(addEffect);
     function addEffect(item, index) {
-        item.classList.toggle("close-icons");
+        item.classList.toggle("close-icons-sidebar");
     }
 
     podeFechar = !podeFechar;
